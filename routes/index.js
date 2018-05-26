75961a2fc3c4e01a0f7a6fc1f95673a12c643635
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-05-26 16:58:45 +0100

    added graphql route with apollo-server

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,36 @@
 const keystone = require('keystone');
-// const middleware = require('./middleware');
-// const importRoutes = keystone.importer(__dirname);
+// const { ApolloServer } = require('apollo-server');
+// const { registerServer } = require('apollo-server-express');
+const cors = require('cors');
+const schema = require('../graphql/schema-compose');
+const jwt = require('express-jwt');
 
-// Common Middleware
-// keystone.pre('routes', middleware.initLocals);
-// keystone.pre('render', middleware.flashMessages);
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
-// Import Route Controllers
-// const routes = {
-// 	views: importRoutes('./views'),
-// };
+const User = keystone.list('User').model;
 
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
 	app.get('/admin', (req, res) => {res.redirect('/keystone')});
 	app.get('/', (req, res) => {res.redirect('/keystone')});
+
+	// Old method to add GraphQL
+	app.use('/graphql', cors(), bodyParser.json(),jwt({
+	  secret: process.env.JWT_SECRET,
+	  credentialsRequired: false,
+	}), graphqlExpress(req=>({
+		schema,
+		context: {
+			user: req.user ? User.findOne({ _id: req.user._id || req.user.id, version: req.user.version}) : Promise.resolve(null)
+		}
+	 })));
+	app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+
+	// New method to add GraphQL
+	// const server = new ApolloServer({
+	// 	schema
+	// });
+	// registerServer({ server, app });
 };
