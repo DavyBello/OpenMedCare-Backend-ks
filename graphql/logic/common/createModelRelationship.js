7c01d7c155398babfae87997aee967aef9ff2a8c
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-06-11 22:31:13 +0100

    create reusable authentication logic

---
+++ graphql/logic/common/createModelRelationship.js
@@ -0,0 +1,24 @@
+const createModelRelationship = exports.createModelRelationship =  ( field, TC ) => {
+	return TC.get('$createOne').wrapResolve(next => async (rp) => {
+		//get sourceUser from resolveParams (rp)
+		const { sourceUser, sourceType } = rp
+		if (sourceUser) {
+			const _field = sourceUser[field]
+			if (Array.isArray(_field)) {
+				//add field to db and get result of createOne resolver
+				const result = await next(rp);
+				sourceUser[field].push(result.recordId);
+				try {
+					await sourceUser.save();
+					return result;
+				} catch (e) {
+					//Placeholder function to stop the field from saving to the db
+					result.record.remove().exec();
+					throw new Error(`Unexpected error adding the document to ${sourceType.toLowerCase()}`);
+				}
+			} else {
+				throw new Error(`Field: ${field} is not a collection`);
+			}
+		}
+	});
+}
