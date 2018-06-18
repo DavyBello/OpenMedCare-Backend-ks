d776a0b999b6862bbfe7f279dff2c38d0482d004
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-06-18 08:26:24 +0100

    update graphql schema

--- graphql/schema-compose.js
+++ graphql/schema-compose.js
@@ -20,6 +20,7 @@ const {
 
 // Get TypeComposers
 const {
+	PlaceHolderTC,
 	UserTC,
 	BenefactorTC,
 	ViewerBenefactorTC
@@ -32,12 +33,12 @@ addViewers();
 
 // Queries
 GQC.rootQuery().addFields({
-	user: UserTC.get('$findOne'),
-	...authAccess({userType: 'Benefactor'}, {
+	user: UserTC.getResolver('findOne'),
+	...authAccess({sourceUserType: 'Benefactor'}, {
 		benefactorIsAuthenticated: UserTC.getResolver('isAuthenticated'),
     viewerBenefactor: ViewerBenefactorTC.getResolver('benefactorAccess'),
 	}),
-	...authAccess({userType: 'Benefactor', isActivated: true}, {
+	...authAccess({sourceUserType: 'Benefactor', isActivated: true}, {
     isActivatedViewerBenefactor: ViewerBenefactorTC.getResolver('benefactorAccess'),
 	}),
 });
@@ -45,6 +46,26 @@ GQC.rootQuery().addFields({
 // Mutations
 GQC.rootMutation().addFields({
 	loginUser: UserTC.getResolver('loginWithEmail'),
+	createBenefactorAccount: PlaceHolderTC.getResolver('underDevelopment'),
+	...authAccess({sourceUserType: 'Benefactor'}, {
+		sendBenefactorActivationLink: UserTC.getResolver('sendUserActivationLink'),
+		// Update Profile Details etc.
+		updateBenefactorDetails: UserTC.getResolver('updateById'),
+		changeBenefactorAccountPassword: UserTC.getResolver('changePassword'),
+		deactivateBenefactorAccount: PlaceHolderTC.getResolver('underDevelopment'),
+		deleteBenefactorAccount: PlaceHolderTC.getResolver('underDevelopment'),
+		createBenefactorAppointment: PlaceHolderTC.getResolver('underDevelopment')
+	}),
+	...authAccess({sourceUserType: 'HPStaff'}, {
+		createTreatmentRecordHPS: PlaceHolderTC.getResolver('underDevelopment')
+		createPrescriptionHPS: PlaceHolderTC.getResolver('underDevelopment')
+		createBenefactorAppointment: PlaceHolderTC.getResolver('underDevelopment')
+	})
+	...authAccess({sourceUserType: 'CHU'}, {
+		createTreatmentRecordCHU: PlaceHolderTC.getResolver('underDevelopment')
+		createPrescriptionCHU: PlaceHolderTC.getResolver('underDevelopment')
+		// createBenefactorAppointment: PlaceHolderTC.getResolver('underDevelopment')
+	})
 });
 
 const schema = GQC.buildSchema();
