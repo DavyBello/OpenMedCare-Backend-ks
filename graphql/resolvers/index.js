8ce4d81bea97aed808eac26f6195d75636fb86d0
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-06-18 08:23:11 +0100

    add placeholder resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,7 @@
 const userResolvers = require('./user')
+const placeHolderResolvers = require('./placeHolder')
 
 module.exports = addResolvers = () => {
   userResolvers();
+  placeHolderResolvers();
 }
